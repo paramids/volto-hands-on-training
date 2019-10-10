b690d20a1d44c769a8498979ecdfd1114d927213
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-10-10 16:08:20 +0200

    Chapter 9

--- src/components/Blocks/MainSlider/Edit.jsx
+++ src/components/Blocks/MainSlider/Edit.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Edit = props => {
+const Edit = ({ data, onChangeTile, tile, selected, properties }) => {
   return <div>I'm the MainSlider edit component!</div>;
 };
 
